dbf09ed1874480234c7a9e44537c486da55544ac
chebpng <yota3301@gmail.com>
2024-10-26 11:51:25 +0700

    Do merge sort and output to file

--- backup/unit1.pas
+++ backup/unit1.pas
@@ -31,6 +31,7 @@ type
     procedure Button3Click(Sender: TObject);
     procedure Button4Click(Sender: TObject);
     procedure Button5Click(Sender: TObject);
+    procedure Button6Click(Sender: TObject);
   private
 
   public
@@ -41,8 +42,8 @@ type
 
 var
   Form1: TForm1;
-  n, ns, z, o, v, sorted, temp, elem: integer;
-  mass,masss:array of integer;
+  n, ns, sn, z, o, v, sorted, temp, elem: integer;
+  mass,masss,smass,result:array of integer;
   trail:string;
 
 implementation
@@ -50,6 +51,74 @@ implementation
 {$R *.lfm}
 
 { TForm1 }
+
+procedure Merge(var arr, arr1: array of Integer; sn, n1: Integer; var res: array of Integer);
+var
+  l, r, k: Integer;
+begin
+  l := 0;
+  r := 0;
+  k := 0;
+
+  while (l < sn) and (r < n1) do
+  begin
+    if arr[l] <= arr1[r] then
+    begin
+      res[k] := arr[l];
+      l := l + 1;
+    end
+    else
+    begin
+      res[k] := arr1[r];
+      r := r + 1;
+    end;
+    k := k + 1;
+  end;
+
+  while l < sn do
+  begin
+    res[k] := arr[l];
+    l := l + 1;
+    k := k + 1;
+  end;
+
+  while r < n1 do
+  begin
+    res[k] := arr1[r];
+    r := r + 1;
+    k := k + 1;
+  end;
+end;
+
+procedure MergeSort(var smass: array of Integer; sn: Integer);
+var
+  mid, i: Integer;
+  leftArr, rightArr, result: array of Integer;
+begin
+  if sn < 2 then
+    Exit;
+
+  mid := sn div 2;
+
+  SetLength(leftArr, mid);
+  SetLength(rightArr, sn - mid);
+
+  for i := 0 to mid - 1 do
+    leftArr[i] := smass[i];
+
+  for i := mid to n - 1 do
+    rightArr[i - mid] := smass[i];
+
+  MergeSort(leftArr, mid);
+  MergeSort(rightArr, sn - mid);
+
+  SetLength(result, sn);
+  Merge(leftArr, rightArr, mid, sn - mid, result);
+
+  for i := 0 to sn - 1 do
+    smass[i] := result[i];
+end;
+
 
 procedure Randomis(var n:integer; var mass:array of integer);
 var elem, z:integer;
@@ -105,7 +174,7 @@ begin
       end;
       masss[o + 1] := temp;
   end;
-  if n <= 40 then
+  if ns <= 40 then
     begin
       for z := 0 to ns-1 do
           begin
@@ -118,6 +187,31 @@ begin
             ListBox2.Items.Add(inttostr(masss[z]));
           end;
     end;
+
+procedure TForm1.Button6Click(Sender: TObject);
+begin
+  //cлияние
+  ListBox3.Items.Clear;
+  sn:=strtoint(Edit1.text);
+  randomize();
+  setlength(smass, sn);
+  setlength(result, sn);
+  Randomis(sn, smass);
+  MergeSort(smass, sn);
+  if sn <= 40 then
+    begin
+      for z := 0 to sn-1 do
+          begin
+               ListBox3.Items.Add(inttostr(smass[z]));
+          end;
+      end
+      else
+          for z := 0 to 39 do
+          begin
+            ListBox3.Items.Add(inttostr(smass[z]));
+          end;
+    end;
+
 
 procedure TForm1.Button3Click(Sender: TObject);
 begin
@@ -150,9 +244,9 @@ begin
         if ListBox2.Items.Count >0 then
         begin
           WriteLn(MyFile, '----------Упорядоченный----------');
-          for i := 0 to ListBox2.Items.Count - 1 do
+          for i := 0 to ns-1 do
           begin
-            WriteLn(MyFile, ListBox2.Items[i]);
+            WriteLn(MyFile, masss[i]);
           end;
         end;
 
@@ -161,9 +255,9 @@ begin
         if ListBox3.Items.Count >0 then
         begin
           WriteLn(MyFile, '----------Слияние----------');
-          for i := 0 to ListBox3.Items.Count - 1 do
+          for i := 0 to sn - 1 do
           begin
-            WriteLn(MyFile, ListBox3.Items[i]);
+            WriteLn(MyFile, smass[i]);
           end;
         end;
 
