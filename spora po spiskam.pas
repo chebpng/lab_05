5c1470e697ee3553e4a9fff4a97e4c091dc80955
chebpng <yota3301@gmail.com>
2024-11-12 19:42:28 +0700

    add examples for linear list

---
+++ spora po spiskam.pas
@@ -0,0 +1,33 @@
+﻿type
+  PNode = ^Node;
+  Node = record
+    data: Integer;
+    next: PNode;
+  end;
+
+var
+  head, current, newNode: PNode;
+  i: Integer;
+begin
+  head := nil; // Начинаем с пустого списка
+
+  // Цикл для создания 10 узлов
+  for i := 1 to 10 do
+  begin
+    New(newNode);         // Создаем новый узел
+    newNode^.data := i;    // Присваиваем данные (например, от 1 до 10)
+    newNode^.next := nil;  // Новый узел пока последний, поэтому next указывает на nil
+
+    if head = nil then
+    begin
+      head := newNode;     // Если это первый узел, то устанавливаем его как голову списка
+      current := head;     // И устанавливаем текущий узел как голову
+    end
+    else
+    begin
+      current^.next := newNode; // Связываем предыдущий узел с новым
+      current := newNode;       // Перемещаем указатель current на новый узел
+    end;
+  end;
+  readln();
+end.
